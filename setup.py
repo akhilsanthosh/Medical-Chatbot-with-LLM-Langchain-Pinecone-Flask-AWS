4f916b1b35b74d78798d2a9dd87b5799d17c9f68
Boddu Sai Akhil <akhil2002boddu@gmail.com>
2025-10-20 11:54:30 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot", 
+    version="0.1.0",
+    author="Boddu Sai Akhil",
+    author_email="akhil2002boddu@gmail.com",
+    packages=find_packages(),
+    install_requires=[]  
+) 
